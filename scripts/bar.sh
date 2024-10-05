193304c2594bd6fbcb27fc6bf59af3dadfb6b66d
ahmedsamyh <ahmedsamyh10@gmail.com>
2024-10-05 21:55:36 +0000

    brightness, vol and kb_lang in bar.sh.

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -6,35 +6,13 @@
 interval=0
 
 # load colors
-. ~/.config/chadwm/scripts/bar_themes/onedark
+. ~/.config/chadwm/scripts/bar_themes/catppuccin
 
 cpu() {
   cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
 
   printf "^c$black^ ^b$green^ CPU"
   printf "^c$white^ ^b$grey^ $cpu_val"
-}
-
-pkg_updates() {
-  #updates=$({ timeout 20 doas xbps-install -un 2>/dev/null || true; } | wc -l) # void
-  updates=$({ timeout 20 checkupdates 2>/dev/null || true; } | wc -l) # arch
-  # updates=$({ timeout 20 aptitude search '~U' 2>/dev/null || true; } | wc -l)  # apt (ubuntu, debian etc)
-
-  if [ -z "$updates" ]; then
-    printf "  ^c$green^    Fully Updated"
-  else
-    printf "  ^c$green^    $updates"" updates"
-  fi
-}
-
-battery() {
-  get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
-  printf "^c$blue^   $get_capacity"
-}
-
-brightness() {
-  printf "^c$red^   "
-  printf "^c$red^%.0f\n" $(cat /sys/class/backlight/*/brightness)
 }
 
 mem() {
@@ -45,19 +23,32 @@ mem() {
 wlan() {
 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
 	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^Connected" ;;
-	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;;
-	esac
+	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;; esac
 }
 
 clock() {
 	printf "^c$black^ ^b$darkblue^ 󱑆 "
-	printf "^c$black^^b$blue^ $(date '+%H:%M')  "
+    printf "^c$black^^b$blue^ $(date '+%d(%a) %h %y %H:%M:%S')"
+}
+
+volume() {
+    printf "^c$black^ ^b$darkblue^ %s " $(volicon)
+    printf "^b$black^ ^c$darkblue^ %s " $(amixer sget Master | grep -o '[0-9]*%' | sed 's/%//' | head -n1)
+}
+
+brightness() {
+  printf "^c$black^^b$red^  "
+  printf "^b$black^^c$red^%s"$(echo "$(xrandr --verbose | awk '/Brightness/ { print $2; exit }') * 100.0" | bc | sed "s/\..*$//g")
+}
+
+kb_lang() {
+  printf "^b$black^^c$green^ $(sb-kblang)"
 }
 
 while true; do
 
-  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
-  interval=$((interval + 1))
+  # [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+  # interval=$((interval + 1))
 
-  sleep 1 && xsetroot -name "$updates $(battery) $(brightness) $(cpu) $(mem) $(wlan) $(clock)"
+  sleep 1 && xsetroot -name "$(volume) $(brightness) $(cpu) $(mem) $(wlan) $(clock) $(kb_lang)"
 done

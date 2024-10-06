b35ec49170d15a63e9f37ffab761eb804ab52abc
ahmedsamyh <ahmedsamyh10@gmail.com>
2024-10-06 09:30:53 +0000

    [bar.sh] Add rec_icon, wifi name and kb_lang.

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -9,46 +9,54 @@ interval=0
 . ~/.config/chadwm/scripts/bar_themes/catppuccin
 
 cpu() {
-  cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+	cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
 
-  printf "^c$black^ ^b$green^ CPU"
-  printf "^c$white^ ^b$grey^ $cpu_val"
+	printf "^c$black^ ^b$green^ CPU"
+	printf "^c$white^ ^b$grey^ $cpu_val"
 }
 
 mem() {
-  printf "^c$blue^^b$black^  "
-  printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
+	printf "^c$blue^^b$black^  "
+	printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
 }
 
 wlan() {
 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^Connected" ;;
-	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;; esac
+	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^" ;;
+	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^" ;;
+    esac
+    ssid=$(nmcli -t -f active,ssid dev wifi | grep -E '^yes' | cut -d\' -f2 | sed 's/.*://g')
+    [ -z "$ssid" ] && ssid="Not Connected"
+	printf "^b$black^ ^c$blue^ %s " $ssid
 }
 
 clock() {
 	printf "^c$black^ ^b$darkblue^ 󱑆 "
-    printf "^c$black^^b$blue^ $(date '+%d(%a) %h %y %H:%M:%S')"
+	printf "^c$black^^b$blue^ $(date '+%d(%a) %h %y %H:%M:%S')"
 }
 
 volume() {
-    printf "^c$black^ ^b$darkblue^ %s " $(volicon)
-    printf "^b$black^ ^c$darkblue^ %s " $(amixer sget Master | grep -o '[0-9]*%' | sed 's/%//' | head -n1)
+	printf "^c$black^ ^b$darkblue^ %s " $(volicon)
+	printf "^b$black^ ^c$darkblue^ %s " $(amixer sget Master | grep -o '[0-9]*%' | sed 's/%//' | head -n1)
 }
 
 brightness() {
-  printf "^c$black^^b$red^  "
-  printf "^b$black^^c$red^%s"$(echo "$(xrandr --verbose | awk '/Brightness/ { print $2; exit }') * 100.0" | bc | sed "s/\..*$//g")
+	printf "^c$black^^b$red^  "
+	printf "^b$black^^c$red^ %s "$(echo "$(xrandr --verbose | awk '/Brightness/ { print $2; exit }') * 100.0" | bc | sed "s/\..*$//g")
 }
 
 kb_lang() {
-  printf "^b$black^^c$green^ $(sb-kblang)"
+	printf "^b$black^^c$green^ $(xkb-switch)"
+}
+
+recording_icon() {
+    printf "^b$black^^c$red^ %s " $(cat /tmp/recordingicon)
 }
 
 while true; do
 
-  # [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
-  # interval=$((interval + 1))
+	# [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+	# interval=$((interval + 1))
 
-  sleep 1 && xsetroot -name "$(volume) $(brightness) $(cpu) $(mem) $(wlan) $(clock) $(kb_lang)"
+    sleep 1 && xsetroot -name "$(recording_icon) $(volume) $(brightness) $(cpu) $(mem) $(wlan) $(clock) $(kb_lang)"
 done
